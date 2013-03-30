fa56a6fb1865a53250813d057adbc92377c7a42f
Max Rubin <merubin14@gmail.com>
2013-03-30 00:53:40 -0300

    Update js_formatter.py
    Adds spaces after commas, unless it is in a quote.  
    
    Bug: cannot handle \" in a quote

--- js_formatter.py
+++ js_formatter.py
@@ -1,43 +1,58 @@
 import sys, string, re, os, fileinput
 
+def readIn():
+	lines = sys.stdin.read()
+	one_line = ''
+	for line in lines:
+		one_line += line.rstrip()
+	return one_line
+
 def printOut(c, ind):
 	if c == ';':
 		sys.stdout.write(c)
-		print
+		sys.stdout.write('\n')
 		sys.stdout.write(ind)
 	elif c == '{':
 		sys.stdout.write(c)
-		print
+		sys.stdout.write('\n')
 		sys.stdout.write(ind)
 	elif c == '}':
-		print
+		sys.stdout.write('\n')
 		sys.stdout.write(ind)
 		sys.stdout.write(c)
-		print
+		sys.stdout.write('\n')
 		sys.stdout.write(ind)
+	elif c == ',':
+		sys.stdout.write(c)
+		sys.stdout.write(' ')
 	else:
 		sys.stdout.write(c)
 
 def main():
-		line = sys.stdin.read()
-		
-		indent = ""
-		lenIndent = 0
-		for c in line:
-			# Add 4 spaces indent
-			if c == '{':
-				print 
-				sys.stdout.write(indent)
-				indent = indent + "    "
-				lenIndent = lenIndent + 4
-			# Remove 4 spaces indent
-			if c == '}' and lenIndent > 0:
-				indent = indent[:lenIndent-4]
-				lenIndent = lenIndent-4
+	line = readIn()
+	
+	indent = ""
+	lenIndent = 0
+	inParen = False
+	for c in line:
+		# Add 4 spaces indent
+		if c == '"':
+			if inParen:
+				inParen = False
+			else:
+				inParen = True
+		if c == '{':
+			sys.stdout.write('\n') 
+			sys.stdout.write(indent)
+			indent = indent + "    "
+			lenIndent = lenIndent + 4
+		# Remove 4 spaces indent
+		if c == '}' and lenIndent > 0:
+			indent = indent[:lenIndent-4]
+			lenIndent = lenIndent-4
+		if inParen:
+			sys.stdout.write(c)
+		else:
 			printOut(c, indent)
-			
-				
-		
-		print 
-		print "=+=+=+=+ Finish +=+=+=+="
+
 main()
